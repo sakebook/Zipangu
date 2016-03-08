bcf08927ae834f32c43ae145ab2e850f767cfd02
Shinya Sakemoto <sakebook@gmail.com>
2016-03-08 13:31:50 +0900

    update sample

--- javasample/src/main/java/com/sakebook/android/sample/javasample/MainActivity.java
+++ javasample/src/main/java/com/sakebook/android/sample/javasample/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.sakebook.android.library.zipangu.Area;
 import com.sakebook.android.library.zipangu.Prefecture;
 import com.sakebook.android.library.zipangu.Segment;
 import com.sakebook.android.library.zipangu.Zipangu;
@@ -16,8 +17,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Zipangu.beginning(this, new Prefecture(0, "カスタム県", "カスタムエリア", Segment.None));
-        Log.d("java", Zipangu.code(100).getName());
-        Log.d("java", Zipangu.name("青").getName());
-        Log.d("java", Zipangu.segment(Segment.West).toString());
+        Log.d("java", Zipangu.code(1).getName());
+        Log.d("java", Zipangu.code(100).getName()); // return default prefecture
+        Log.d("java", Zipangu.name("北海道").toString());
+        Log.d("java", Zipangu.name("東京都").toString());
+        Log.d("java", Zipangu.name("青森").toString()); // return default prefecture
+        Log.d("java", Zipangu.segment(Segment.North).toString());
+        Log.d("java", Zipangu.area(Area.中部).toString());
+        Log.d("java", Zipangu.all().toString());
     }
 }
--- kotlinSample/src/main/java/com/sakebook/android/sample/kotlinsample/MainActivity.kt
+++ kotlinSample/src/main/java/com/sakebook/android/sample/kotlinsample/MainActivity.kt
@@ -17,9 +17,10 @@ class MainActivity : AppCompatActivity() {
         Log.d("kotlin", Zipangu.code(1).name)
         Log.d("kotlin", Zipangu.code(100).name) // return default prefecture
         Log.d("kotlin", Zipangu.name("北海道").toString())
-        Log.d("kotlin", Zipangu.name("青森").toString()) // return default prefecture
         Log.d("kotlin", Zipangu.name("東京都").toString())
-        Log.d("kotlin", Zipangu.segment(Segment.East).toString())
+        Log.d("kotlin", Zipangu.name("青森").toString()) // return default prefecture
+        Log.d("kotlin", Zipangu.segment(Segment.North).toString())
         Log.d("kotlin", Zipangu.area(Area.中部).toString())
+        Log.d("kotlin", Zipangu.all().toString())
     }
 }
